84697afca12ac7e57791ac7b0a9a65bc06173428
Omied <omied@example.com>
2024-12-12 17:59:35 +0100

    17:59

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import data from "./data.json"
 
 function App() { 
   return (
-    <div>
+    <div className='container'>
       {data.map((item, index) => (
+      <div className='item'>
         <h2 key={index}>{item.title}</h2>
+        <p>{item.description}</p>
+      </div>
       ))}
     </div>
   );
