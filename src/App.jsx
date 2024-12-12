935aad5ae329988f0a2ff5138a6b32dd5fda1a3c
timbaking <timbaking>
2024-12-12 16:59:21 +0100

    titles in h2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import data from "./data.json"
 
 function App() { 
-  return (<div> Hallo </div>
-  )
- 
+  return (
+    <div>
+      {data.map((item, index) => (
+        <h2 key={index}>{item.title}</h2>
+      ))}
+    </div>
+  );
 }
 
 export default App
